65c282d5cb9292d973196cefced643ac4968cc4e
Bekithemba Ngulube <bekteema@Bekis-MacBook-Pro.local>
2018-09-03 00:26:13 +0000

    second commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,10 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
-//console.log(myHeading);
+var myImage = document.querySelector('#myprofilepic');
 
-var myImage = document.querySelector('#afc');
-
-myImage.onclick = function() {
+myImage.onmouseover = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/arsenal-logo.svg') {
-      myImage.setAttribute ('src','images/ferrari-logo.png');
+    if(mySrc === 'images/beki.png') {
+      myImage.setAttribute ('src','images/beki-black-and-white.png');
     } else {
-      myImage.setAttribute ('src','images/arsenal-logo.svg');
+      myImage.setAttribute ('src','images/beki.png');
     }
-}
-
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
-
-function setUserName() {
-  var myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  myHeading.textContent = 'Hello World. My name is '  + myName;
-}
-
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Hello World. My name is ' + storedName;
-}
-
-myButton.onclick = function() {
-  setUserName();
 }
